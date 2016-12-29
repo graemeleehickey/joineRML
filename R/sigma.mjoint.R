b9dbb7f3f42ad6221891d325eed897d23eeb5208
Graeme Hickey <graemeleehickey@gmail.com>
2016-12-29 17:42:28 +0000

    Patch to get package to build on r-oldrel-windows-ix86+x86_64

--- R/sigma.mjoint.R
+++ R/sigma.mjoint.R
@@ -1,6 +1,22 @@
-#' Print \code{mjoint} object
+#' Extract residual standard deviation(s) from an \code{mjoint} object
 #'
-#' @keywords internal
+#' Extract residual standard deviation(s) from an \code{mjoint} object.
+#'
+#' @inheritParams confint.mjoint
+#'
+#' @author Graeme L. Hickey (\email{graeme.hickey@@liverpool.ac.uk})
+#' @keywords methods
+#' @seealso \code{\link[lme4]{sigma}} in the \strong{lme4} package.
+#'
+#' @rawNamespace if (getRversion() >= '3.3.0') importFrom(stats,sigma) else
+#'   importFrom(lme4,sigma)
+#' @references
+#'
+#' Pinheiro JC, Bates DM. \emph{Mixed-Effects Models in S and S-PLUS.} New York:
+#' Springer Verlag; 2000.
+#'
+#' @return a number (standard deviation) if \eqn{K=1} (univariate model), or a
+#'   vector if \eqn{K>1} (multivariate model).
 #' @export
 sigma.mjoint <- function(object, ...) {
 
