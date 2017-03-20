90753f620b40d6d199d7ae2be24d9c8542f6f8d2
Graeme Hickey <graemeleehickey@gmail.com>
2017-03-20 14:13:01 +0000

    Used QR decomp to invert matrix to get vcov as unstable otherwise for splines

--- R/mjoint.R
+++ R/mjoint.R
@@ -765,7 +765,7 @@ mjoint <- function(formLongFixed, formLongRandom, formSurv, data, survData = NUL
   out$finalnMC <- nMC # not same as control nMC (used for burn-in phase)
   if (conv && pfs) {
     out$vcov <- postFitCalcs$ses
-    out$SE.approx <- sqrt(diag(solve(out$vcov)))
+    out$SE.approx <- sqrt(diag(qr.solve(out$vcov)))
     out$Eb <- postFitCalcs$Eb # Posterior RE means
     out$Vb <- postFitCalcs$Vb # Posterior RE variances
     out$dmats <- list(l = l, t = t, z = z)
